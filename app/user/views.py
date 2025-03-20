ffaeb85e204341a242baf93e5b3c6df3566814fd
Jorge Solano <jsolano@iventmobile.com>
2025-03-20 19:59:41 +0100

    73. Implement manage user API

--- app/user/views.py
+++ app/user/views.py
@@ -2,11 +2,37 @@
 Views for the user API.
 """
 
-from rest_framework import generics
+from rest_framework import generics, authentication, permissions
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
 
-from user.serializers import UserSerializer
+from user.serializers import (
+    UserSerializer,
+    AuthTokenSerializer,
+)
 
 
 class CreateUserView(generics.CreateAPIView):
     """Create a new user in the system."""
     serializer_class = UserSerializer
+
+
+class CreateTokenView(ObtainAuthToken):
+    """Create a new auth token for the user."""
+    # Normally ObtainAuthToken only uses username and password.
+    # This line is to override the behaviour to use email and password.
+    serializer_class = AuthTokenSerializer
+    # Optional, to render the page in the browser.
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+
+
+class ManageUserView(generics.RetrieveUpdateAPIView):
+    """Manage the authenticated user."""
+    serializer_class = UserSerializer
+    authentication_classes = [authentication.TokenAuthentication]
+    permission_classes = [permissions.IsAuthenticated]
+    http_method_names = ['get', 'put', 'patch']  # This will block POST
+
+    def get_object(self):
+        """Retrieve and return authenticated user."""
+        return self.request.user
