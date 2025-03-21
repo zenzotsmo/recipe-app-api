fa159c66cc42d4243bd21ed119aaefdaa5be3170
Jorge Solano <jsolano@iventmobile.com>
2025-03-21 14:35:20 +0100

    86. Implement create recipe API,

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -27,3 +27,6 @@ class RecipeViewSet(viewsets.ModelViewSet):
         if self.action == 'list':
             return serializers.RecipeSerializer
         return self.serializer_class
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
